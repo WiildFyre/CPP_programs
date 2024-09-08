af86d6ef147325bff7278fa52026a057503637bf
WiildFyre <132829680+WiildFyre@users.noreply.github.com>
2024-09-08 22:45:41 +0530

    Add files via upload
    Insertion sort for integers

---
+++ ins_sort_int.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void bubble_sort(int n,int a[])
+{
+	int i,j,temp;
+	for(i=0;i<n;i++)
+	{
+		temp = a[i];
+		j = i-1;
+		while(j>=0 && temp<a[j])
+		{
+			a[j+1] = a[j];
+			j--;
+		}
+		a[j+1] = temp;
+	}
+	for(i=0;i<n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+}
+int main()
+{
+	int ar[] = {5,4,2,9,12,6,14,0};
+	int size = sizeof(ar)/sizeof(ar[0]);
+	bubble_sort(size,ar);
+	return 0;
+}
